1bb519a2c11c386dc02cf8fd0938ec0980f4901b
yabool2001 <mzemlo.pl@gmail.com>
2022-05-11 23:26:18 +0200

    aaaa

--- read_bin_from_string.py
+++ read_bin_from_string.py
@@ -1,13 +1,11 @@
 import struct
+import gzip
 
 data = b'\x01\x00\x00\x00\x02\x00\x00\x00'
+data_x , data_y = struct.unpack ( '2I' , data )
+print ( data_x , data_y )
 
-x , y = struct.unpack ( '2I' , data )
-print ( x , y )
-
-file = open ( 'string_with_bin.txt' , 'r' )
-line = file.readline ()
-data_line = bytes (line,'ascii')
-print ( data_line )
-#x , y = struct.unpack ( '2I' , file_data )
-#print ( x , y )
+s = open ( 'string_with_bin.txt' , 'r' ) .readline ()
+b = eval ( s )
+b_x , b_y = struct.unpack ( '2I' , b )
+print ( b_x , b_y )
